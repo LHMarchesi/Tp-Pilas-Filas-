c7df83159b2e44233a17bd7454f9ef8a3a92b0f7
LautaroMarchesi <lautarohmarchesi@gmail.com>
2024-08-24 17:03:50 -0300

    SectionManager
    La cantidad de secciones ahora se puede ajustar desde el inspector

--- Assets/Scritps/ContainerManager.cs
+++ Assets/Scritps/ContainerManager.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class ContainerManager : MonoBehaviour
 {
-    [SerializeField] private GameObject containerObj;
-    [SerializeField] private GameObject obj;
-    [SerializeField] Stack<GameObject> containerStack = new Stack<GameObject>();
+    [SerializeField] private GameObject item;
     [SerializeField] private int maxItems;
+    private Stack<GameObject> containerStack = new Stack<GameObject>();
     private Vector3 startPosition;
     private Vector3 offset = new Vector3(0, 0, .1f);
-    private Vector3 pos;
 
     private void Start()
     {
-        startPosition = containerObj.transform.position; //Toma la posicion del container y la ajusta
+        startPosition = this.transform.position; //Toma la posicion del container y la ajusta
         startPosition.y += .4f;
         startPosition.z -= .4f;
     }
@@ -24,7 +22,7 @@ public class ContainerManager : MonoBehaviour
         if (containerStack.Count < maxItems)
         {
             Vector3 newPos = startPosition + offset * containerStack.Count; // a la posicion incial le suma el offset y lo multiplica por la canitidad de items dentro del stack
-            GameObject item = Instantiate(obj, newPos, Quaternion.identity);
+            GameObject item = Instantiate(this.item, newPos, Quaternion.identity);
             containerStack.Push(item);
         }
     }
